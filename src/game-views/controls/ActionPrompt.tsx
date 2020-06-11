329593c8eb2f334283d35cce49c6f709b0d5068a
Vincent Siao <vincent@asana.com>
2020-06-10 22:36:07 -0700

    Add react-transition-group to slide in/out prompts

--- src/game-views/controls/ActionPrompt.tsx
+++ src/game-views/controls/ActionPrompt.tsx
@@ -4,18 +4,26 @@ import { connect } from "react-redux";
 import { PromptState } from "../../game-data/prompts/PromptState";
 import ChooseActionPrompt from "./ChooseActionPrompt";
 import { AppState } from "../../store/AppState";
+import { CSSTransition } from "react-transition-group";
 
 interface Props {
     actionPrompt: PromptState;
 }
 
 const ActionPrompt: React.FunctionComponent<Props> = props => {
-    if (props.actionPrompt === null) {
-        return null;
-    }
-    return <div className="ActionPrompt">
-        {renderPrompt(props.actionPrompt, props)}
-    </div>;
+    return <CSSTransition
+        in={props.actionPrompt !== null}
+        timeout={200}
+        classNames="ActionPrompt"
+        mountOnEnter
+        unmountOnExit
+    >
+        <div className="ActionPrompt">
+            {props.actionPrompt === null
+                ? null
+                : renderPrompt(props.actionPrompt, props)}
+        </div>
+    </CSSTransition>;
 };
 
 const renderPrompt = (prompt: Exclude<PromptState, null>, props: Props) => {
